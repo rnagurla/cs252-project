74a09f18ccdb371f8c6f87c9b927a819e4d5dedc
Brian Talbert <btalber@purdue.edu>
2017-12-05 20:37:37 -0500

    Colors

--- public/js/findPrices.js
+++ public/js/findPrices.js
@@ -4,7 +4,13 @@ var destination;
 function myFunc() {
     alert("js working");
 
-    var start = document.getElementById('start');
-    var destination = document.getElementById('destination');
-    
+    var s = document.getElementById('start');
+    var d = document.getElementById('destination');
+
+    var start = s.value;
+    var destination = d.value;
+
+    alert(start);
+    alert(destination);
+
 }
