e64eeab97d2e3fed5ed6037202e175441b04f45e
Brian Talbert <btalber@purdue.edu>
2017-12-05 20:32:09 -0500

    More js

--- public/js/findPrices.js
+++ public/js/findPrices.js
@@ -3,4 +3,8 @@ var destination;
 
 function myFunc() {
     alert("js working");
+
+    var start = document.getElementById('start');
+    var destination = document.getElementById('destination');
+    
 }
