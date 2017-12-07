3a35efc12100b56e840f0832e7fe820e87344460
Brian Talbert <btalber@purdue.edu>
2017-12-06 20:37:29 -0500

    Google API doestn work

--- public/js/findPrices.js
+++ public/js/findPrices.js
@@ -3,7 +3,7 @@ var destination;
 var taxi_price;
 var uber_price;
 var lyft_price;
-
+var xhr = new XMLHttpRequest();
 
 
 function myFunc() {
@@ -22,13 +22,26 @@ function myFunc() {
     uberPrices();
     lyftPrices();
     taxi();
+    var url = "https://maps.googleapis.com/maps/api/directions/origin=" + start + "&destination=" + destination + "&key=AIzaSyDJBmjA5VQP8dtPI9MfCyub-DAXjm5jvF0";
+alert(url);
+    xhr.open('GET', url, true);
+    xhr.send();
+    xhr.addEventListener("readystatechange", processRequest, false);
+
+    //document.location = 'Results.html';
 }
 
+function processRequest() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        var response = JSON.parse(xhr.responseText);
+         alert(xhr.responseText);
+    }
+}
 
 function uberPrices() {
     alert(start);
     alert(destination)
-    document.location = 'Results.html';
+
 }
 
 function lyftPrices() {
@@ -36,5 +49,5 @@ function lyftPrices() {
 }
 
 function taxi() {
-    
+
 }
